17c82b6d383916dc5cab56520e7af749a6e7d13c
rvevangelista211 <rvevangelista92@gmail.com>
2015-07-26 14:45:44 +0000

    Third Subprojects

---
+++ public/script/banking.js
@@ -0,0 +1,37 @@
+var name = "Online Banking";
+document.getElementById("demo1").innerHTML = name;
+
+var name= "Online banking (OLB) is an electronic payment system that enables customers of a financial institution to conduct financial transactions on a website operated by the institution, such as a retail bank, virtual bank, credit union or building society. Online banking is also referred as Internet banking, e-banking, virtual banking and by other terms.";
+document.getElementById("demo2").innerHTML = name;
+
+var name = "To access online banking, a customer would go to the financial institution's secured website, and enter the online banking facility using the customer number and password previously setup. Some financial institutions have set up additional security steps for access to online banking, but there is no consistency to the approach adopted.";
+document.getElementById("demo3").innerHTML = name;
+
+var name = "History";
+document.getElementById("demo4").innerHTML = name;
+
+var name = "The precursor for the modern home online banking services were the distance banking services over electronic media from the early 1980s. The term 'Online' became popular in the late '80s and referred to the use of a terminal, keyboard and TV (or monitor) to access the banking system using a phone line. 'Home banking' can also refer to the use of a numeric keypad to send tones down a phone line with instructions to the bank. Online services started in New York in 1981 when four of the city's major banks (Citibank, Chase Manhattan, Chemical and Manufacturers Hanover) offered home banking services using the videotex system. Because of the commercial failure of videotex these banking services never became popular except in France where the use of videotex (Minitel) was subsidised by the telecom provider and the UK, where the Prestel system was used. For more information about the latter see Online banking in the U.K.";
+document.getElementById("demo5").innerHTML = name;
+
+var name ="When the clicks-and-bricks euphoria hit in the late 1990s, many banks began to view Web-based banking as a strategic imperative. The attraction of banks to online banking are fairly obvious: diminished transaction costs, easier integration of services, interactive marketing capabilities, and other benefits that boost customer lists and profit margins. Additionally, Web banking services allow institutions to bundle more services into single packages, thereby luring customers and minimizing overhead.";
+document.getElementById("demo6").innerHTML = name;
+
+var name = "A mergers-and-acquisitions wave swept the financial industries in the mid-and late 1998s, greatly expanding banks' customer bases. Following this, banks looked to the Web as a way of maintaining their customers and building loyalty. A number of different factors are causing bankers to shift more of their business to the virtual realm.";
+document.getElementById("demo7").innerHTML = name;
+
+var name ="While financial institutions took steps to implement e-banking services in the mid-1990s, many consumers were hesitant to conduct monetary transactions over the web. It took widespread adoption of electronic commerce, based on trailblazing companies such as America Online, Amazon.com and eBay, to make the idea of paying for items online widespread. By 2000, 80 percent of U.S. banks offered e-banking. Customer use grew slowly. At Bank of America, for example, it took 10 years to acquire 2 million e-banking customers. However, a significant cultural change took place after the Y2K scare ended. In 2001, Bank of America became the first bank to top 3 million online banking customers, more than 20 percent of its customer base. In comparison, larger national institutions, such as Citigroup claimed 2.2 million online relationships globally, while J.P. Morgan Chase estimated it had more than 750,000 online banking customers. Wells Fargo had 2.5 million online banking customers, including small businesses. Online customers proved more loyal and profitable than regular customers. In October 2001, Bank of America customers executed a record 3.1 million electronic bill payments, totaling more than $1 billion. In 2009, a report by Gartner Group estimated that 47 percent of U.S. adults and 30 percent in the United Kingdom bank online.";
+document.getElementById("demo8").innerHTML = name;
+
+var name ="Advantages of Online Banking"
+document.getElementById("demo9").innerHTML = name;
+
+var name ="1.Permanent access to the bank  2.Lower transaction costs/general cost reductions 3.Access anywhere"
+document.getElementById("demo10").innerHTML = name;
+
+var name ="Security"
+document.getElementById("demo11").innerHTML = name;
+
+var name ="Security of a customer's financial information is very important, without which online banking could not operate. Similarly the reputational risks to the banks themselves are important.[5] Financial institutions have set up various security processes to reduce the risk of unauthorized online access to a customer's records, but there is no consistency to the various approaches adopted. The use of a secure website has become almost universally adopted."
+document.getElementById("demo12").innerHTML = name;
+
+
